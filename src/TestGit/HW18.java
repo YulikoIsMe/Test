e53cfd8fd9c047cff6d06719921a1d9a32ceeeb3
YulikoIsMe <yuliko@me.com>
2023-04-25 17:45:26 -0400

    Yulia

---
+++ src/TestGit/HW18.java
@@ -0,0 +1,18 @@
+package TestGit;
+
+public class HW18 {
+    public static void main(String[] args) {
+
+
+        int counter = 20;
+        while (counter <= 50) {
+            System.out.println(counter);
+            counter += 3;
+      /* for (int i = 20; i <=50; i ++) {
+           if (i%2!=0){
+               System.out.println(i+"");*/
+        }
+
+    }
+}
+
